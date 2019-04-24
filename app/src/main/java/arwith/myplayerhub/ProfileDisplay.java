f6bea7972c1367981e8e8a27b589139244d2cc87
Seb Rabau <strabau@gmail.com>
2019-04-24 17:21:47 +0100

    layout updates

--- app/src/main/java/arwith/myplayerhub/ProfileDisplay.java
+++ app/src/main/java/arwith/myplayerhub/ProfileDisplay.java
@@ -3,11 +3,14 @@ package arwith.myplayerhub;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class ProfileDisplay extends AppCompatActivity {
+public class ProfileDisplay extends AppCompatActivity implements View.OnClickListener {
 
     String displayname = "Unknown User";
+    LinearLayout popup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,16 @@ public class ProfileDisplay extends AppCompatActivity {
 
         Intent intent = getIntent();
         displayname = intent.getStringExtra("displayName");
+
+        findViewById(R.id.addCard).setOnClickListener(this);
+        findViewById(R.id.bnetAdd).setOnClickListener(this);
+        findViewById(R.id.epicAdd).setOnClickListener(this);
+        findViewById(R.id.originAdd).setOnClickListener(this);
+        findViewById(R.id.steamAdd).setOnClickListener(this);
+        findViewById(R.id.closePopup).setOnClickListener(this);
+        findViewById(R.id.changeUsername).setOnClickListener(this);
+
+        popup = findViewById(R.id.popup);
     }
 
     @Override
@@ -24,5 +37,27 @@ public class ProfileDisplay extends AppCompatActivity {
 
         TextView usernameText = (TextView) findViewById(R.id.Username);
         usernameText.setText(displayname);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+
+        if(i == R.id.addCard) {
+            popup.setVisibility(View.VISIBLE);
+        } else if(i == R.id.closePopup) {
+            popup.setVisibility((View.GONE));
+        } else if(i == R.id.steamAdd) {
+
+        } else if(i == R.id.originAdd) {
+
+        } else if(i == R.id.bnetAdd) {
+
+        } else if(i == R.id.epicAdd) {
+
+        } else if(i == R.id.changeUsername) {
+            Intent intent = new Intent(this, ChangeUsername.class);
+            startActivity(intent);
+        }
     }
 }
